fd8287dcb5bbf45742e1805984af4ce5be2ce5c7
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-09-05 18:46:32 +0900

    Update guild.py

--- cogs/guild.py
+++ cogs/guild.py
@@ -5,12 +5,10 @@ class sarver(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        chs=[c for c in guild.channels]
-        ch=chs[0]
-        em = discord.Embed(title="**導入ありがとうございます**",description="このbotは管理者しか使うことができません", color=discord.Color.blue())
+        ch=guild.system_channel
+        em = discord.Embed(title="**導入ありがとうございます**",description="このbotは管理者しか使うことができません\nPrefixは`e!`です", color=discord.Color.blue())
         await ch.send(embed=em)
 
 def setup(bot):
