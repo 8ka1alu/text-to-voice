03652a72e67aa7ed677c375ca2d87660b65ef8ad
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-11-17 13:23:30 +0900

    Update guild.py

--- cogs/guild.py
+++ cogs/guild.py
@@ -1,23 +1,15 @@
 import discord
 from discord.ext import commands
 
-class sarver(commands.Cog):
+class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        ch=guild.system_channel
-        em = discord.Embed(title="**導入ありがとうございます**",description="このbotは管理者しか使うことができません\nPrefixは`e!`です", color=discord.Color.blue())
-        if ch != None:
-            await ch.send(embed=em)
-
     @commands.command()
-    async def invite(self, ctx):
-        invite="https://discord.com/api/oauth2/authorize?client_id=751714059652562966&permissions=8&scope=bot"
-        sinvite="https://discord.gg/Jejvvpn"
-        em = discord.Embed(title="**こちらから導入できます**",description=f"[このBOTの招待はこちら](<{invite}>)｜[サポート鯖はこちら](<{sinvite}>)", color=discord.Color.blue())
-        await ctx.send(embed=em)
+    @commands.has_permissions(admin=True)
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def test(self, ctx):
+        print("test")
 
 def setup(bot):
-    bot.add_cog(sarver(bot))
+    bot.add_cog(test(bot))
