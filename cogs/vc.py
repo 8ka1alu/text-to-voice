79f827036dc390c9bd217ad98ea771ac7891e9d2
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-11-17 15:57:14 +0900

    Create vc.py

---
+++ cogs/vc.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+class VC(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def test(self, ctx):
+        print("test")
+
+def setup(bot):
+    bot.add_cog(VC(bot))
