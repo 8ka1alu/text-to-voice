70d2c1401e626150c0026b75a93432ebd7114e02
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-11-17 16:08:45 +0900

    Update vc.py

--- cogs/vc.py
+++ cogs/vc.py
@@ -1,15 +1,44 @@
 import discord
 from discord.ext import commands
+import os
+
+if not discord.opus.is_loaded():
+    discord.opus.load_opus("heroku-buildpack-libopus")
 
 class VC(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    @commands.cooldown(1, 30, commands.BucketType.user)
-    async def test(self, ctx):
-        print("test")
+    @commands.command(aliases=["connect","summon","con"])
+    async def join(self, ctx):
+        voice_state = ctx.author.voice
+
+        if (not voice_state) or (not voice_state.channel):
+            await ctx.send("先にボイスチャンネルに入っている必要があります。")
+            return
+
+        channel = voice_state.channel
+
+        await channel.connect()
+
+        embed = discord.Embed(title="**接続完了**", description=f"接続チャンネル名```{channel.name}```")
+        embed.timestamp = ctx.message.created_at
+        await ctx.send(embed = embed)
+
+    @commands.command(aliases=["disconnect","bye","dis"])
+    async def leave(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+
+        if not voice_client:
+            await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+            return
+
+        await voice_client.disconnect()
+
+        embed = discord.Embed(title="**ボイスチャンネルから切断しました**", description=None)
+        embed.timestamp = ctx.message.created_at
+        await ctx.send(embed = embed)
+
 
 def setup(bot):
     bot.add_cog(VC(bot))
